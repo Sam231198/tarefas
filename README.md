b3188dabf5e8f89dc702147de4ba3ae1351c9fbd
Samuel Marques <samuelmarques231198@gmail.com>
2021-03-22 11:42:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TECNOLOGIAS
+
+Instale essas ferramentas para rodar o projeto:
+
+- [Java 11](https://jdk.java.net/java-se-ri/11)
+- [PostgreSQL](https://www.postgresql.org/download/ "PostgreSQL")
+
+Framework utilizado:
+
+- [Sping Boot 4 MVC](https://spring.io/projects/spring-boot)
+
---
+++ micro_service_tarefas/src/main/java/com/MicroServiceTarefasApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceTarefasApplication.class, args);
+	}
+
+}

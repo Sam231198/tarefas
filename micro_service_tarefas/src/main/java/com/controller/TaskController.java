0bbaf6bc06dc6891fa3229ac502aedb5f6f6309e
Samuel Marques <samuelmarques231198@gmail.com>
2021-03-23 10:27:55 -0300

    finalizando back-end

--- micro_service_tarefas/src/main/java/com/controller/TaskController.java
+++ micro_service_tarefas/src/main/java/com/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,33 +22,32 @@ public class TaskController {
 
 	private TaskRepository taskRepository;
 
-	@SuppressWarnings("unchecked")
-	@GetMapping("/")
-	public ResponseEntity<Task> index() {
-		return (ResponseEntity<Task>) taskRepository.findAll();
+	@GetMapping
+	public List<Task> index() {
+		return (List<Task>) taskRepository.findAll();
 	}
 
-	@GetMapping("/")
+	@GetMapping("/{id}")
 	public Optional<Task> getTask(@PathVariable("id") Integer id) {
 		return taskRepository.findById(id);
 	}
-	
-	@PostMapping("/")
+
+	@PostMapping
 	public Task add(@RequestBody Task task) {
 		return taskRepository.save(task);
 	}
 
-	@PutMapping("/")
-	public Task update(@PathVariable("id") Integer id, @RequestBody Task taskUpdate) {
+	@PutMapping("/{id}")
+	public Task update(@PathVariable("id") int id, @RequestBody Task taskUpdate) {
 		Task task = taskRepository.findById(id).orElseThrow();
-		
-		task.setTitulo(taskUpdate.getTitulo());
-		task.setDetalhe(taskUpdate.getDetalhe());
-		
+
+		task.setTitulo((taskUpdate.getTitulo() != null) ? taskUpdate.getTitulo() : task.getTitulo());
+		task.setDetalhe((taskUpdate.getDetalhe() != null) ? taskUpdate.getDetalhe() : task.getDetalhe());
+
 		return taskRepository.save(task);
 	}
-	
-	@DeleteMapping("/")
+
+	@DeleteMapping("/{id}")
 	public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
 		taskRepository.deleteById(id);
 		return ResponseEntity.ok().build();
--- micro_service_tarefas/src/main/java/com/model/Task.java
+++ micro_service_tarefas/src/main/java/com/model/Task.java
@@ -10,7 +10,7 @@ public class Task {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
+	private int id;
 	
 	private String titulo;
 	
